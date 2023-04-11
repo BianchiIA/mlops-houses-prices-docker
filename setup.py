09fce6462a2fbd24d37c489bc93f044edee489c0
Vinicius Bianchi Soares <vinibianch@outlook.com>
2023-04-10 21:16:04 -0300

    Experiments in MLFLOW and init cookiecutter repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Esse projeto contém o um modelo regressivo simples, baseado em xgboost, orquestrado pelo MLFLOW. Consta também as etapas de CI/CD com github actions',
+    author='Vinicius Bianchi',
+    license='',
+)
